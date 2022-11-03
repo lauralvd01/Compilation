5be7ed0610e53c12c9a787eb589654c94bf7677c
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-11-03 15:34:17 +0100

    Pretty printer fonctionnel ! (pas encore nécessaire de se casser la tête avec l'enregistrement des variables ... Par contre recherches à faire sur les floats avant de commencer l'assembleur !

--- compiloQuat.py
+++ compiloQuat.py
@@ -1,7 +1,8 @@
 import lark
 
 grammaire = lark.Lark(r"""
-exp : SIGNED_NUMBER "\+" SIGNED_NUMBER "i\+" SIGNED_NUMBER "j\+" SIGNED_NUMBER "k"     -> exp_nombre
+exp : SIGNED_FLOAT "+" SIGNED_FLOAT "i" "+" SIGNED_FLOAT "j" "+" SIGNED_FLOAT "k"     -> exp_nombre
+| SIGNED_NUMBER                  -> exp_entier
 | IDENTIFIER                     -> exp_var
 | exp OPBIN exp                  -> exp_opbin
 | "(" exp ")"                    -> exp_par
@@ -22,12 +23,102 @@ IDENTIFIER : /[a-zA-Z][a-zA-Z0-9]*/
 OPBIN : /[+\-*>]/
 %import common.WS
 %import common.SIGNED_NUMBER
+%import common.SIGNED_FLOAT
 %ignore WS
 """,start="prg")
 
 op = {'+' : 'add', '-' : 'sub', '*' : 'mult'}
-coordonnees = [0,0,0,0] # VALIDE pour n'enregistrer qu'1 SEUL quaternion seulement !
-## TODO : revoir l'exemple du prof pour voir comment il fait pour sauvegarder des valeurs !
+
+def pp_exp(e):
+    if e.data == "exp_nombre":
+        return f"{e.children[0].value} + {e.children[1].value}i + {e.children[2].value}j + {e.children[3].value}k"
+    elif e.data == "exp_var":
+        return e.children[0].value
+    elif e.data == "exp_par":
+        return f"({pp_exp(e.children[0])})"
+    elif e.data == "exp_opbin":
+        return f"{pp_exp(e.children[0])} {e.children[1].value} {pp_exp(e.children[2])}"
+    elif e.data == "exp_reel":
+        return f"re({pp_exp(e.children[0])})"
+    elif e.data == "exp_im":
+        return f"im({pp_exp(e.children[0])})"
+    elif e.data == "exp_i":
+        return f"{pp_exp(e.children[0])}.i"
+    elif e.data == "exp_j":
+        return f"{pp_exp(e.children[0])}.j"
+    elif e.data == "exp_k":
+        return f"{pp_exp(e.children[0])}.k"
+    elif e.data == "exp_entier":
+        return f"{e.children[0]}"
+    else:
+        return "Cas non autorisé actuellement"
+
+def pp_assignation(c):
+    assert(c.data == "assignation")
+    str = f"{c.children[0].value} = "
+    return str
+
+def pp_com(c):
+    if c.data == "assignation":
+        return f"{c.children[0].value} = {pp_exp(c.children[1])};"
+    elif c.data == "if":
+        x = f"\n{indent(pp_bcom(c.children[1]))}"
+        return f"if ({pp_exp(c.children[0])}) {{{x}}}"
+    elif c.data == "while":
+        x = f"\n{indent(pp_bcom(c.children[1]))}"
+        return f"while ({pp_exp(c.children[0])}) {{{x}}}"
+    elif c.data == "print":
+        # TODO
+        return f"print({pp_exp(c.children[0])})"
+
+def pp_bcom(bc):
+    return "\n".join([pp_com(c) for c in bc.children])
+
+def pp_var_list(vl):
+    return ", ".join([t.value for t in vl.children])
+
+def pp_prg(p):
+    L = pp_var_list(p.children[0])
+    C = pp_bcom(p.children[1])
+    R = pp_exp(p.children[2])
+    return "main(%s) {\n%s    return(%s);\n}" % (L, indent(C), R)
+
+def indent(s: str, amplitude = 4):
+    """
+    Indents a string, i.e adds amplitude * " " at the beginning of each line.
+    """
+    indent_s = amplitude * " "
+    words = s.split("\n")
+    if not words[-1]:
+        words.pop()
+    return indent_s + f"\n{indent_s}".join(words) + "\n"
+
+
+print(pp_prg(grammaire.parse("""
+main() {
+    x = 1. + 2.0i + 3.0j + 4.0k;
+    if (x) {
+        y = 4. + 2.0i +3.0j + 4.0k;
+        while(y){
+            i = i + 1. + 2.i + 3.j + 4.k;
+        }
+    }
+    return (x);
+}
+""")))
+
+
+#def vars_exp(e):
+#    if e.data  == "exp_nombre":
+#        return set()
+#    elif e.data ==  "exp_var":
+#        return { e.children[0].value }
+#    elif e.data == "exp_par":
+#        return vars_exp(e.children[0])
+#    else:
+#        L = vars_exp(e.children[0])
+#        R = vars_exp(e.children[2])
+#        return L | R
 
 ###
 # def asm_exp(e):
@@ -49,46 +140,11 @@ coordonnees = [0,0,0,0] # VALIDE pour n'enregistrer qu'1 SEUL quaternion seuleme
 #        """
 ###
 
-def pp_exp(e):
-    global coordonnees
-    if e.data == "exp_nombre":
-        return f"{e.children[0].value} + {e.children[1].value}i + {e.children[2].value}j + {e.children[3].value}k"
-    elif e.data == "exp_var":
-        return e.children[0].value
-    elif e.data == "exp_par":
-        return f"({pp_exp(e.children[0])})"
-    elif e.data == "exp_opbin":
-        return f"{pp_exp(e.children[0])} {e.children[1].value} {pp_exp(e.children[2])}"
-    elif e.data == "exp_reel":
-        return f"{coordonnees[0]}"
-    elif e.data == "exp_im":
-        return f"{coordonnees[1]}i + {coordonnees[2]}j + {coordonnees[3]}k"
-    elif e.data == "exp_i":
-        return f"{coordonnees[1]}i"
-    elif e.data == "exp_j":
-        return f"{coordonnees[2]}j"
-    elif e.data == "exp_k":
-        return f"{coordonnees[3]}k"
-    else:
-        return "Cas non autorisé actuellement"
-
-#def vars_exp(e):
-#    if e.data  == "exp_nombre":
-#        return set()
-#    elif e.data ==  "exp_var":
-#        return { e.children[0].value }
-#    elif e.data == "exp_par":
-#        return vars_exp(e.children[0])
-#    else:
-#        L = vars_exp(e.children[0])
-#        R = vars_exp(e.children[2])
-#        return L | R
-
-cpt = 0
-def next():
-    global cpt
-    cpt += 1
-    return cpt
+#cpt = 0
+#def next():
+#    global cpt
+#    cpt += 1
+#    return cpt
 
 #def asm_com(c):
 #    if c.data == "assignation":
@@ -129,19 +185,6 @@ def next():
 #        call printf
 #        """
 
-def pp_com(c):
-    if c.data == "assignation":
-        return f"{c.children[0].value} = {pp_exp(c.children[1])};"
-    elif c.data == "if":
-        x = f"\n{pp_bcom(c.children[1])}"
-        return f"if ({pp_exp(c.children[0])}) {{{x}}}"
-    elif c.data == "while":
-        x = f"\n{pp_bcom(c.children[1])}"
-        return f"while ({pp_exp(c.children[0])}) {{{x}}}"
-    elif c.data == "print":
-        return f"print({pp_exp(c.children[0])})"
-
-
 #def vars_com(c):
 #    if c.data == "assignation":
 #        R = vars_exp(c.children[1])
@@ -156,17 +199,11 @@ def pp_com(c):
 #def asm_bcom(bc):
 #    return "".join([asm_com(c) for c in bc.children])
 
-def pp_bcom(bc):
-    return "\n".join([pp_com(c) for c in bc.children])
-
 #def vars_bcom(bc):
 #    S = set()
 #    for c in bc.children:
 #        S = S | vars_com(c)
 #    return S
-
-def pp_var_list(vl):
-    return ", ".join([t.value for t in vl.children])
 
 #def asm_prg(p):
 #    f = open("moule.asm")
@@ -197,19 +234,13 @@ def pp_var_list(vl):
 #    R = vars_exp(p.children[2])
 #    return L | C | R
 
-def pp_prg(p):
-    L = pp_var_list(p.children[0])
-    C = pp_bcom(p.children[1])
-    R = pp_exp(p.children[2])
-    return "main( %s ) { %s return(%s);\n}" % (L, C, R)
 
-
-ast = grammaire.parse("""
-    main(x,y,z){
-        c = 1 + 3i + 4j + 5k;
-        print(c.i)
-    }
-""")
+#ast = grammaire.parse("""
+#    main(x,y,z){
+#        c = 1 + 3i + 4j + 5k;
+#        print(c.i)
+#    }
+#""")
 #asm = asm_prg(ast)
 #f = open("quat.asm", "w")
 #f.write(asm)
